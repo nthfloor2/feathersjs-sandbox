b42ae3e404101331fab8038a604fcb3daa158bfe
Nathan Floor <nathan.floor@jembi.org>
2019-10-23 06:07:51 +0200

    Updates to feathers default app generator

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # feathers-chat
 
-> 
+> Chat bot built with Feathersjs
 
 ## About
 
@@ -14,7 +14,8 @@ Getting up and running is as easy as 1, 2, 3.
 2. Install your dependencies
 
     ```
-    cd path/to/feathers-chat; npm install
+    cd path/to/feathers-chat
+    npm install
     ```
 
 3. Start your app
@@ -36,22 +37,9 @@ $ npm install -g @feathersjs/cli          # Install Feathers CLI
 
 $ feathers generate service               # Generate a new Service
 $ feathers generate hook                  # Generate a new Hook
-$ feathers generate model                 # Generate a new Model
 $ feathers help                           # Show all commands
 ```
 
 ## Help
 
 For more information on all the things you can do with Feathers visit [docs.feathersjs.com](http://docs.feathersjs.com).
-
-## Changelog
-
-__0.1.0__
-
-- Initial release
-
-## License
-
-Copyright (c) 2018
-
-Licensed under the [MIT license](LICENSE).
--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,8 @@
-const assert = require('assert');
-const rp = require('request-promise');
+const axios = require('axios');
 const url = require('url');
 const app = require('../src/app');
 
-const port = app.get('port') || 3030;
+const port = app.get('port') || 8998;
 const getUrl = pathname => url.format({
   hostname: app.get('host') || 'localhost',
   protocol: 'http',
@@ -11,45 +10,56 @@ const getUrl = pathname => url.format({
   pathname
 });
 
-describe('Feathers application tests', () => {
-  before(function(done) {
-    this.server = app.listen(port);
-    this.server.once('listening', () => done());
+describe('Feathers application tests (with jest)', () => {
+  let server;
+
+  beforeAll(done => {
+    server = app.listen(port);
+    server.once('listening', () => done());
   });
 
-  after(function(done) {
-    this.server.close(done);
+  afterAll(done => {
+    server.close(done);
   });
 
-  it('starts and shows the index page', () => {
-    return rp(getUrl()).then(body =>
-      assert.ok(body.indexOf('<html>') !== -1)
-    );
+  it('starts and shows the index page', async () => {
+    expect.assertions(1);
+
+    const { data } = await axios.get(getUrl());
+
+    expect(data.indexOf('<html lang="en">')).not.toBe(-1);
   });
 
-  describe('404', function() {
-    it('shows a 404 HTML page', () => {
-      return rp({
-        url: getUrl('path/to/nowhere'),
-        headers: {
-          'Accept': 'text/html'
-        }
-      }).catch(res => {
-        assert.equal(res.statusCode, 404);
-        assert.ok(res.error.indexOf('<html>') !== -1);
-      });
+  describe('404', () => {
+    it('shows a 404 HTML page', async () => {
+      expect.assertions(2);
+      try {
+        await axios.get(getUrl('path/to/nowhere'), {
+          headers: {
+            'Accept': 'text/html'
+          }
+        });
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status).toBe(404);
+        expect(response.data.indexOf('<html>')).not.toBe(-1);
+      }
     });
 
-    it('shows a 404 JSON error without stack trace', () => {
-      return rp({
-        url: getUrl('path/to/nowhere'),
-        json: true
-      }).catch(res => {
-        assert.equal(res.statusCode, 404);
-        assert.equal(res.error.code, 404);
-        assert.equal(res.error.message, 'Page not found');
-        assert.equal(res.error.name, 'NotFound');
-      });
+    it('shows a 404 JSON error without stack trace', async () => {
+      expect.assertions(4);
+      
+      try {
+        await axios.get(getUrl('path/to/nowhere'));
+      } catch (error) {
+        const { response } = error;
+
+        expect(response.status).toBe(404);
+        expect(response.data.code).toBe(404);
+        expect(response.data.message).toBe('Page not found');
+        expect(response.data.name).toBe('NotFound');
+      }
     });
   });
 });
